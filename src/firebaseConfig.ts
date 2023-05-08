5ca6d830744acc85fd50f9d1a6604af75f003c27
felipebravo <felipe_bravo@hotmail.com>
2023-05-08 16:34:19 -0300

    chore: add firebase config, frameworks and create vite project

---
+++ src/firebaseConfig.ts
@@ -0,0 +1,21 @@
+import { initializeApp } from 'firebase/app';
+import { getAuth } from 'firebase/auth';
+import { getDatabase } from 'firebase/database';
+import { getStorage } from 'firebase/storage';
+
+const firebaseConfig = {
+	apiKey: process.env.FIREBASE_API_KEY,
+	authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+	projectId: process.env.FIREBASE_PROJECT_ID,
+	storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+	messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+	appId: process.env.FIREBASE_APP_ID,
+	measurementId: process.env.FIREBASE_MEASUREMENT_ID,
+	databaseURL: process.env.FIREBASE_DATABASE_URL,
+};
+
+const firebase = initializeApp(firebaseConfig);
+
+export const auth = getAuth(firebase);
+export const database = getDatabase(firebase);
+export const storage = getStorage(firebase);
